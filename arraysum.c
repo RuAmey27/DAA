427ecfbc8e87925db1efde0be546c13c7f20b041
RuAmey27 <ameypande2709@gmail.com>
2024-01-11 15:10:02 +0530

    First DAA task

---
+++ arraysum.c
@@ -0,0 +1,29 @@
+/**
+ * @file arraysum.c
+ * @author Amey Pande (Ameypande2709@gmail.com)
+ * @brief  This program helps to find the sum of array element using recursion
+ * @version 0.1
+ * @date 2024-01-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+/*Aim : Find the sum of array elements using the recursion*/
+#include <stdio.h>
+
+int sum(int *a, int size, int index) {
+    if (index < size) {
+        return a[index] + sum(a, size, index + 1);
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int a[5] = {10, 20, 30, 40, 50};
+    int len = sizeof(a) / sizeof(a[0]);
+    int addition = sum(a, len, 0);//here  initial parameters are passed
+    printf("%d", addition);
+
+    return 0;
+}
---
+++ maxmin.c
@@ -0,0 +1,32 @@
+/**
+ * @file maxmin.c
+ * @author Amey Pande (Ameypande2709@gmail.com)
+ * @brief  This program is help to find miximum and minimum element from given array
+ * @version 0.1
+ * @date 2024-01-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+/* Aim: Find the minimum and maximum elements in the array*/
+#include <stdio.h>
+int main() {
+  int arr[] = {5, 3, 9, 2, 8, 6, 1, 7, 4, 0};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int max = arr[0];
+  int min = arr[0];
+
+  for (int i = 1; i < n; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  printf("Maximum element: %d\n", max);
+  printf("Minimum element: %d\n", min);
+
+  return 0;
+}
