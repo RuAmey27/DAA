af26e887f0d1bae258a1c6a245ee79d2bc36cf58
RuAmey27 <ameypande2709@gmail.com>
2024-01-12 00:21:53 +0530

    First day of codechef problem

---
+++ compare.c
@@ -0,0 +1,11 @@
+/**
+ * @file compare.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+/*Aim : Use recursion to find the maximum and minimum element of array*/
